d3d518eec58601cb1a1970a17e2fe39b4a3d61d5
Dranikf <kobfedsur@gmail.com>
2019-10-30 08:46:02 +0300

    lab5 start, belarus map included

---
+++ lab5/STATION.h
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "common_funcks.h"
+
+using namespace std;
+
+
+class STATION
+{
+private:
+
+    position    st_pos;
+    string      statinonName;
+    
+public:
+
+    STATION(string);
+    ~STATION();
+
+    void showStationInfo();
+    position getPosition();
+};
---
+++ lab5/main.cpp
@@ -0,0 +1,138 @@
+#include <SFML/Graphics.hpp>
+//#include "STATION.h"
+#include "TRAIN.h"
+#include <fstream>
+
+vector<STATION> stations;
+vector<TRAIN>   trains;
+
+const float stationRadius = 3.f;
+const float trainRadius = 5.f;
+
+bool readStationData(string fileName){
+
+    ifstream file(fileName);
+
+    if(!file.is_open())
+        return false;
+
+    string stationStr;
+
+    while (!file.eof())
+    {
+        getline(file, stationStr);
+        stations.push_back(STATION(stationStr));
+        
+    }
+    
+        
+    file.close();
+
+    return true;
+
+}
+
+void drawStations(sf::RenderWindow * window){
+
+    sf::CircleShape station_graphicks(stationRadius);	
+	station_graphicks.setFillColor(sf::Color::Black);
+    position tempPosition;
+
+    for (int i = 0; i < stations.size(); i++){
+        tempPosition = stations[i].getPosition();
+
+        station_graphicks.setPosition(tempPosition.x, tempPosition.y); 
+        window->draw(station_graphicks);
+    }
+}
+
+void createTrains(){
+
+    trains.push_back(TRAIN(0, 1, sf::Color::Red));
+    trains.push_back(TRAIN(2, 2, sf::Color::Blue));
+
+}
+
+void drawTrains(sf::RenderWindow * window){
+
+    sf::CircleShape station_graphicks(trainRadius);	
+    position tempPosition;
+
+        for (int i = 0; i < trains.size(); i++){
+        tempPosition = stations[trains[i].getStationIndex()].getPosition();
+
+        station_graphicks.setFillColor(trains[i].getTrainColor());
+        station_graphicks.setPosition(tempPosition.x, tempPosition.y); 
+        window->draw(station_graphicks);
+    }
+
+}
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 300), "My window");
+
+    sf::RectangleShape rectangle(sf::Vector2f(400.f, 300.f));
+
+        
+    sf::Texture konturTexture;
+    if (!konturTexture.loadFromFile("kontur.jpg"))
+    {
+        return 0;
+    }
+
+    rectangle.setTexture(&konturTexture);
+
+
+    sf::Vector2i localPosition;
+
+    sf::CircleShape station_graphicks(3.f);	
+	station_graphicks.setFillColor(sf::Color::Black);
+
+    if(!readStationData("stations.inf"))
+        return 0;
+
+    createTrains();
+
+    while (window.isOpen())
+    {
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::KeyReleased){
+                if (event.key.code == sf::Keyboard::Q)
+                    trains[0].moveTrain(&stations);
+                if (event.key.code == sf::Keyboard::W)
+                    trains[1].moveTrain(&stations);
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+
+                trains[0].moveTrain(&stations, sf::Mouse::getPosition(window));
+                
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Right)){
+
+                trains[1].moveTrain(&stations, sf::Mouse::getPosition(window));
+                
+            }
+                
+        }
+        window.clear(sf::Color::White);
+
+        window.draw(rectangle);
+
+        drawStations(&window);
+        drawTrains(&window);
+
+        window.display();
+    }
+
+    return 0;
+}
